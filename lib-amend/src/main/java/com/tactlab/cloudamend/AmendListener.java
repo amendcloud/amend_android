d0fe866613ac8fde601180198e58ac340dc20df8
deepaktact <deepak@tact.co.in>
2016-12-22 13:27:52 +0530

    Version update

--- lib-amend/src/main/java/com/tactlab/cloudamend/AmendListener.java
+++
@@ -1,9 +0,0 @@
-package com.tactlab.cloudamend;
-
-/**
- * Created by deepak on 28-09-2016.
- */
-public interface AmendListener {
-    void onSuccess(int statusCode, int reqCode, String imageId) throws Exception;
-    void onError(int statusCode, int reqCode, Exception e);
-}
